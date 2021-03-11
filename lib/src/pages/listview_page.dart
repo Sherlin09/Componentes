48a921f65a4e796b4fa0a5c3d790b2030cdaf30d
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2021-03-10 22:52:24 -0600

    listView

---
+++ lib/src/pages/listview_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  List<int> _listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        final imagen = _listaNumeros[index];
+        return FadeInImage(
+          image: NetworkImage('https://picsum.photos/500/300?random=$imagen'),
+          placeholder: AssetImage('assets/loadingJ.gif'),
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider y checks'),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            _crearCheckBox(),
+            _crearSwitch(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+        activeColor: Colors.indigoAccent,
+        label: 'Tamaño de la imagen',
+        // divisions: 20,
+        value: _valorSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: (_bloquearCheck)
+            ? null
+            : (valor) {
+                setState(() {
+                  _valorSlider = valor;
+                });
+              });
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://2.bp.blogspot.com/_EZ16vWYvHHg/S-BkcsYBUrI/AAAAAAAAMI8/d35vD4Pj8mw/s1600/www.BancodeImagenesGratuitas.com-Fantasticas-8.jpg'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+
+  Widget _crearCheckBox() {
+    // return Checkbox(
+    //   value: _bloquearCheck,
+    //   onChanged: (valor) {
+    //     setState(() {
+    //       _bloquearCheck = valor;
+    //     });
+    //   },
+    // );
+    return CheckboxListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+}
